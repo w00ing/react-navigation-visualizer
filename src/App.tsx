37d72276343940e76164624db3f9da556eb542c5
Woo-yeong Kim <kimwy1997@gmail.com>
2025-01-23 23:00:41 +0900

    chore: create sample app

--- src/App.tsx
+++ src/App.tsx
@@ -11,36 +11,76 @@ import {
   Node,
 } from "@xyflow/react";
 
-import { TooltipNode } from "@/components/tooltip-node";
+import { NumNode } from "@/components/nodes/num-node";
+import { SumNode } from "@/components/nodes/sum-node";
+
+import { DataEdge } from "@/components/data-edge";
+import { useCallback } from "react";
 
 const nodeTypes = {
-  tooltip: TooltipNode,
+  num: NumNode,
+  sum: SumNode,
 };
 
 const initialNodes: Node[] = [
-  {
-    id: "1",
-    position: { x: 0, y: 0 },
-    data: {
-      label: "Hover me",
-      tooltip: {
-        label: "Boo!",
-        position: Position.Bottom,
-      },
-    },
-    type: "tooltip",
-  },
+  { id: "a", type: "num", data: { value: 0 }, position: { x: 0, y: 0 } },
+  { id: "b", type: "num", data: { value: 0 }, position: { x: 0, y: 200 } },
+  { id: "c", type: "sum", data: { value: 0 }, position: { x: 300, y: 100 } },
+  { id: "d", type: "num", data: { value: 0 }, position: { x: 0, y: 400 } },
+  { id: "e", type: "sum", data: { value: 0 }, position: { x: 600, y: 400 } },
 ];
 
-const initialEdges: Edge[] = [];
+const edgeTypes = {
+  data: DataEdge,
+};
+
+const initialEdges: Edge[] = [
+  {
+    id: "a->c",
+    type: "data",
+    data: { key: "value" },
+    source: "a",
+    target: "c",
+    targetHandle: "x",
+  },
+  {
+    id: "b->c",
+    type: "data",
+    data: { key: "value" },
+    source: "b",
+    target: "c",
+    targetHandle: "y",
+  },
+  {
+    id: "c->e",
+    type: "data",
+    data: { key: "value" },
+    source: "c",
+    target: "e",
+    targetHandle: "x",
+  },
+  {
+    id: "d->e",
+    type: "data",
+    data: { key: "value" },
+    source: "d",
+    target: "e",
+    targetHandle: "y",
+  },
+];
 
 function App() {
   const [nodes, , onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
-  const onConnect: OnConnect = params => {
-    setEdges(edges => addEdge(params, edges));
-  };
+  const onConnect: OnConnect = useCallback(
+    params => {
+      setEdges(edges =>
+        addEdge({ type: "data", data: { key: "value" }, ...params }, edges),
+      );
+    },
+    [setEdges],
+  );
 
   return (
     <div className="h-screen w-screen p-8">
@@ -51,6 +91,7 @@ function App() {
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
         nodeTypes={nodeTypes}
+        edgeTypes={edgeTypes}
         fitView
       />
     </div>
